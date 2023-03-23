4ec404eedcd17cddb69ccafcb2d4f34b3eef746d
wangbo <781278657@qq.com>
2023-03-23 16:13:04 +0800

    feat: 🎸 配置 mysql

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -6,4 +6,21 @@ export default {
   koa: {
     port: 7001,
   },
+  // todo step-three（配置 mysql）
+  typeorm: {
+    dataSource: {
+      default: {
+        type: 'mysql',
+        host: '',
+        port: 3306,
+        username: '',
+        password: '',
+        database: undefined,
+        synchronize: false, // 注意：这个要写 false，因为已经有数据了
+        logging: false,
+        // 自动扫描 entity 配置
+        entities: ['**/entity/*.entity{.ts}'],
+      },
+    },
+  },
 } as MidwayConfig;
