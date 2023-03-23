4bd79f79c1a25e4a9a059ba3ce1fdc5a70a1035c
wangbo <781278657@qq.com>
2023-03-23 22:27:27 +0800

    feat: 🎸 增删改查：查询

--- src/controller/person.controller.ts
+++ src/controller/person.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Inject } from '@midwayjs/core';
-import { PersonService } from '../service/person.service';
+import {Controller, Get, Inject, Query} from '@midwayjs/core';
+import {PersonService} from '../service/person.service';
 
 @Controller('/')
 export class PersonController {
   @Inject()
   personService: PersonService;
 
-  @Get('/person')
-  async getPerson() {
+  // 增删改查：查询（采用 restful 蛇形命名法）
+  @Get('/search_person')
+  async getPerson(@Query('name') name: string) {
     try {
-      return await this.personService.findPerson();
+      return await this.personService.findPerson(name);
     } catch (e) {
       console.error(e);
     }
--- src/service/person.service.ts
+++ src/service/person.service.ts
@@ -1,17 +1,21 @@
 // todo step-four（编写 Service）
-import { Provide } from '@midwayjs/core';
-import { InjectEntityModel } from '@midwayjs/typeorm';
-import { Person } from '../entity/person.entity';
-import { Repository } from 'typeorm';
+import {Provide} from '@midwayjs/core';
+import {InjectEntityModel} from '@midwayjs/typeorm';
+import {Person} from '../entity/person.entity';
+import {Repository} from 'typeorm';
 
 @Provide()
 export class PersonService {
   @InjectEntityModel(Person)
   personModel: Repository<Person>;
 
-  async findPerson() {
+  async findPerson(name: string) {
     try {
-      return await this.personModel.find({});
+      return await this.personModel.find({
+        where: {
+          'user_name': name
+        }
+      });
     } catch (e) {
       console.error(e);
     }
