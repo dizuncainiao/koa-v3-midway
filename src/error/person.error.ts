0f4994c195c44df0652ebf5812c997ef0a1314bc
wangbo <781278657@qq.com>
2023-03-24 22:39:57 +0800

    feat: 🎸 错误处理

---
+++ src/error/person.error.ts
@@ -0,0 +1,12 @@
+import {MidwayError} from '@midwayjs/core'
+
+export class PersonEmptyDataError extends MidwayError {
+  constructor(err?: Error) {
+    super('person data is empty', {
+      cause: err
+    });
+    if (err?.stack) {
+      this.stack = err.stack
+    }
+  }
+}
--- src/service/person.service.ts
+++ src/service/person.service.ts
@@ -1,8 +1,9 @@
 // todo step-four（编写 Service）
-import { Provide } from '@midwayjs/core';
-import { InjectEntityModel } from '@midwayjs/typeorm';
-import { Person } from '../entity/person.entity';
-import { Like, Repository } from 'typeorm';
+import {Provide} from '@midwayjs/core';
+import {InjectEntityModel} from '@midwayjs/typeorm';
+import {Person} from '../entity/person.entity';
+import {Like, Repository} from 'typeorm';
+import {PersonEmptyDataError} from "../error/person.error";
 
 @Provide()
 export class PersonService {
@@ -13,10 +14,10 @@ export class PersonService {
     try {
       const condition = name
         ? {
-            where: {
-              user_name: Like(`%${name}%`), // 模糊查询，包含
-            },
-          }
+          where: {
+            user_name: Like(`%${name}%`), // 模糊查询，包含
+          },
+        }
         : {};
 
       return await this.personModel.find(condition);
@@ -28,8 +29,8 @@ export class PersonService {
   async savePerson(data: Person) {
     try {
       await this.personModel.save(data);
-    } catch (e) {
-      console.error(e);
+    } catch (err) {
+      throw new PersonEmptyDataError(err)
     }
   }
 
